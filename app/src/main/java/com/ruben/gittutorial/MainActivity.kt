c729196a907ca3e911ab48a11edc08d98cdb1a75
rbracho <rubenbracho77@gmail.com>
2023-01-03 09:09:05 -0500

    initial commit

---
+++ app/src/main/java/com/ruben/gittutorial/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ruben.gittutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        print("Este es un ejemplo")
+    }
+}
