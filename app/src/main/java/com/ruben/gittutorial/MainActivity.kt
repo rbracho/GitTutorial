e10407fa9738def56eaf980c3c2ef42a7912c7e8
rbracho <rubenbracho77@gmail.com>
2023-01-03 09:16:56 -0500

    included print 2

--- app/src/main/java/com/ruben/gittutorial/MainActivity.kt
+++ app/src/main/java/com/ruben/gittutorial/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("Este es un otro ejemplo")
+        print("Este es una linea nueva")
+        print("linea adicional")
     }
 }
